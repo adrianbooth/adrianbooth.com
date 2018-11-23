88fd1e27293e6b795c291c56173bc6796b89fcdd
adrianbooth <adz001@msn.com>
2018-11-23 12:44:32 +0000

    Add Alexa.NET project
    https://github.com/timheuer/alexa-skills-dotnet

---
+++ src/adrianbooth.com/adrianbooth.com/Controllers/HelloPeterController.cs
@@ -0,0 +1,15 @@
+﻿using Alexa.NET;
+using Microsoft.AspNetCore.Mvc;
+
+namespace adrianbooth.com.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloPeterController : Controller
+    {
+        [HttpPost]
+        public dynamic Post(dynamic value)
+        {
+           return ResponseBuilder.Tell("Peter is the best baby.");
+        }
+    }
+}
